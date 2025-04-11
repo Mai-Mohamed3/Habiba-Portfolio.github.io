9274d7c4e7ef407a1f86496a09f44d9f42b5c6b0
Mai Mohamed <mai.mo.alii3@gmail.com>
2025-04-12 01:33:49 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Navbar from ".Components/Navbar";
-import Hero from ".Components/Hero";
-import AboutMe from ".Components/AboutMe";
-import ".Styles/global.css";
-import Education from ".Components/Education";
-import Skills from ".Components/Skills";
-import Profiles from ".Components/Profiles";
-import Portfolio from ".Components/Portfolio";
-import Contact from ".Components/Contact";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import AboutMe from "./Components/AboutMe";
+import "./Styles/global.css";
+import Education from "./Components/Education";
+import Skills from "./Components/Skills";
+import Profiles from "./Components/Profiles";
+import Portfolio from "./Components/Portfolio";
+import Contact from "./Components/Contact";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
